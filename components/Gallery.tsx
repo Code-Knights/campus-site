9afcfed3035cdfa163df02032f5edfcf8fd12cf5
miyucutee <kroniikbar@gmail.com>
2023-03-16 18:07:24 +0700

    style(gallery): add animation gallery
    pretty cool

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,39 +1,31 @@
-import style from "./gallery.module.css";
+import styles from "./gallery.module.css";
 import useSwr from "swr";
+import cn from "../utils/cn";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-const Gallery = () => {
-  const styles = {
-    container: {
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 1fr)",
-      gridGap: "10px",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "850px",
-      height: "850px",
-    },
-    image: {
-      width: "100%",
-      ObjectFit: "cover",
-      height: "100%",
-    },
-  };
-  const { data, error } = useSwr("/api/images", fetcher);
+interface GalleryProps {
+  url: string
+  start: number 
+  end: number 
+}
+
+const Gallery = (props:GalleryProps) => {
+  const { data, error } = useSwr(props.url, fetcher);
 
   if (error) return <div>failed to load</div>;
 
   return (
-    <div style={styles.container}>
-      {data?.map((image: string, index: number) => (
-        <img
-          className={style.fit}
-          key={index}
-          src={image}
-          alt={image}
-          style={styles.image}
-        />
+    <div className={styles.container}>
+      {data?.slice(props.start, props.end).map((image: string, index: number) => (
+        <figure className={styles.figure}>
+          <img
+            className={cn(styles.fit, styles.img)}
+            key={index}
+            src={image}
+            alt={image}
+          />
+        </figure>
       ))}
     </div>
   );
